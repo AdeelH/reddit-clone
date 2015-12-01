17e0bf368b9bc637f1836cf3eb3ceaf60dce209e
Adeel <adeel_hassan93@hotmail.com>
2015-12-01 12:32:59 +0500

    Fixed new comment bug

--- php/new_comm.php
+++ php/new_comm.php
@@ -3,12 +3,14 @@
     require("../includes/config.php");
     session_start();
 
+    // apologize(print_r($_POST));
+
     // sanity checks
 	if (empty($_POST['text']))		apologize("Cannot post empty comment.");
 	if (empty($_GET["pid"]))		apologize("Post-id missing.");
 
 	// make post
-	if (make_comment(get_post($_GET["pid"]), $_POST["text"], isset($_POST["parent_id"]) ? $_POST["parent_id"]:null) === false)
+	if (make_comment(get_post($_GET["pid"]), $_POST["text"], !empty($_POST["parent_id"]) ? $_POST["parent_id"]:null) === false)
 		apologize("Failed to submt comment.");
 
 	redirect("post.php?soc=".$_GET["soc"]."&pid=".$_GET["pid"]);
